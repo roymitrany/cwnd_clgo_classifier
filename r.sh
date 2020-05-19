a31a685830099124cba58577cdb565c2b04473c1
roymitrany <roym@technion.ac.il>
2020-05-19 16:56:38 +0300

    Make Dean's code work with Pycharm Fix some of the critical bugs No Major modifications in code structure

--- r.sh
+++ r.sh
@@ -19,13 +19,13 @@ for i in ${@//[[\],]}; do
 	fi
 	echo "hello" | netcat "${i}" 2345
 done
-echo "queue size:" > queue_size.txt
+echo "queue size:" > /tmp/qqq.txt
 
 count=0
-until (($count==50)); do
-	#sleep 0.1 # Waits for 1 second before checking again.
-	netcat -l 2345
-	tc -s qdisc ls dev r-srv | grep -oP "Sent\s+\K\w+" >> queue_size.txt
+until (($count==500)); do
+	sleep 0.1 # Waits for 1 second before checking again.
+	#netcat -l 2345
+	tc -s qdisc show dev r-srv| grep backlog  >> /tmp/qqq.txt
 	count=$((count + 1))
 done
 # tc -s qdisc ls dev r-eth3 | awk '{for(i=1;i<=NF;i++) if ($i=="Sent") print $(i+1)}' > queue_size.txt
