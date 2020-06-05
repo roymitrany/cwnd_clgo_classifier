83d626bddef2dd8f125509e40e5f3e3b97e8fe5d
DeanCarmel <60920257+DeanCarmel@users.noreply.github.com>
2020-06-05 17:29:37 +0300

    Add files via upload

---
+++ queue_statistics.py
@@ -0,0 +1,38 @@
+import signal
+import subprocess
+from subprocess import Popen
+from sys import argv
+import re
+import time
+
+run = True
+
+def signal_handler(signal, frame):
+    global run
+    #print ("exiting")
+    run = False
+
+
+if __name__ == '__main__':
+    queue_len_bytes_list = []
+    queue_len_packets_list = []
+    assert argv != 3, "argv was %d. add ifname and filename as parameters" % len(argv)
+    if_name = argv[1]
+    results_filename = argv[2]
+    signal.signal(signal.SIGINT, signal_handler)
+    while run:
+        p = Popen(["/sbin/tc", "-s", "qdisc", "show",  "dev", if_name], stdout=subprocess.PIPE, universal_newlines=True)
+        output = p.communicate()
+        match = re.search("backlog\s+(\d+[kK]?)b\s+(\d+)p", output[0])
+        if match:
+            if output[0].find("K") == -1:
+                queue_len_bytes_list.append (match.group(1))
+            else:
+                num = int(match.group(1)[0:-1])*1000
+                queue_len_bytes_list.append(str(num))
+            queue_len_packets_list.append(match.group(2))
+        time.sleep(0.01)
+    # SIGINT was called. Save all results in a file:
+    with open(results_filename, 'w') as outfile:
+        for num in queue_len_bytes_list:
+            outfile.write(num + "\n")
---
+++ simulation_implementation.py
@@ -0,0 +1,138 @@
+import os
+import json
+import re
+
+from enum import Enum
+from signal import SIGINT
+from time import sleep
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import setLogLevel
+from mininet.net import Mininet
+from datetime import datetime
+from mininet.node import OVSController
+from mininet.util import pmonitor
+
+from simulation_topology import SimulationTopology
+
+class TcpAlgorithm(Enum):
+    reno = 1
+    cubic = 2
+    vegas = 3
+    westwood = 4
+    bbr = 5
+
+class Iperf3Simulator:
+    def __init__(self, simulation_topology, simulation_name, seconds=10):
+        """
+        :param simulation_topology: The topology class to be used
+        :param simulation_name: the results folder will contain the test name
+        :param seconds: The test duration
+        """
+        self.simulation_topology = simulation_topology
+        self.net = Mininet(simulation_topology, controller=OVSController, link=TCLink, autoSetMacs=True)
+        self.seconds = seconds
+        self.simulation_name = simulation_name
+        tn = datetime.now()
+        time_str = str(tn.month) + "." + str(tn.day) + "." + str(tn.year) + "@" + str(tn.hour) + "-" + str(tn.minute) + "-" + str(tn.second)
+
+        # Create results directory, with name includes num of reno num of vegas and time
+        self.res_dirname = os.path.join(os.getcwd(), "results", self.simulation_name + "@" + time_str)        
+        os.mkdir(self.res_dirname, 0o777);
+
+        # Set results file names
+        self.iperf_out_filename = os.path.join(self.res_dirname, "iperf_output.txt")
+        self.rtr_q_filename = os.path.join(self.res_dirname, "rtr_q.txt")
+
+    def SetCongestionControlAlgorithm(self, host_idx, tcp_algo):
+        """
+        :param tcp_algo: a member of TcpAlgorithm enum e.g. TcpAlgorithm.westwood
+        """
+
+        host = self.simulation_topology.host_list[host_idx]
+        cmd = 'echo %s > /proc/sys/net/ipv4/tcp_congestion_control' % tcp_algo.name
+        self.net.getNodeByName(host).cmd(cmd)
+
+    def StartSimulation(self):
+        self.net.start()
+        #CLI(self.net)
+        # Modify TCP algorithms (because iperf3 does not support vegas in -C parameter):
+        for host_number in range(0, self.simulation_topology.num_of_reno_hosts):
+            self.SetCongestionControlAlgorithm(host_number, TcpAlgorithm.reno)
+        for host_number in range(self.simulation_topology.num_of_reno_hosts,
+                                 self.simulation_topology.num_of_reno_hosts + self.simulation_topology.num_of_vegas_hosts):
+            self.SetCongestionControlAlgorithm(host_number, TcpAlgorithm.vegas)
+
+        srv = self.net.getNodeByName(self.simulation_topology.srv)
+        srv_ip = srv.IP()
+        popens = {}
+        srv_procs = []
+        
+        # Auxiliary loop- including iperf for the servers and initializing monotoring functions using "tcpdump":
+        client_counter = 0
+        for client in self.simulation_topology.host_list:
+            test_port = (5201 + client_counter)
+            # In iperf3, each test should have its own server. We have to terminate them at the end,
+            # otherwise they are stuck in the system, so we keep the proc nums in a list.
+            srv_cmd = 'iperf3 -s -p %d&' % test_port
+            srv_procs.append(srv.popen(srv_cmd))
+
+            """
+            # Throughput measuring- using tshark:
+            # Running tshark on client side, saving to pcap file (a separate pcap file for each client):
+            pcap_file_name = os.path.join(self.res_dirname, "host_%s.pcap" % client)
+            cmd = 'tshark -i host_int -f "port %d" -w %s -F libpcap&' % (test_port, pcap_file_name)
+            (self.net.getNodeByName(client)).cmd(cmd)
+            # Running tshark on server side, saving to pcap file (a separate pcap file for each client):
+            pcap_file_name = os.path.join(self.res_dirname, "server_%s.pcap" % client)
+            cmd = 'tshark -i srv-r -f "port %d" -w %s&' % (test_port, pcap_file_name)
+            srv.cmd(cmd)
+            """
+            
+            # Throughput measuring- using tcpdump:
+            # Running tcpdump on client side, saving to txt file (a separate txt file for each client):
+            capture_filename = os.path.join(self.res_dirname, "client_%s.txt" % client)
+            cmd = "tcpdump 'tcp port %d'>%s&" % (test_port, capture_filename)
+            (self.net.getNodeByName(client)).cmd(cmd)
+            # Running tcpdump on server side, saving to txt file (a separate txt file for each client):
+            capture_filename = os.path.join(self.res_dirname, "server_%s.txt" % client)
+            cmd = "tcpdump 'tcp port %d'>%s&" % (test_port, capture_filename)
+            srv.cmd(cmd)
+            client_counter += 1
+            
+        sleep(10)        
+        # Traffic generation loop:
+        client_counter = 0
+        for client in self.simulation_topology.host_list:
+            cmd = 'iperf3 -c %s -t %d -p %d' % (srv_ip, self.seconds, 5201 + client_counter)
+            popens[client] = (self.net.getNodeByName(client)).popen(cmd)
+            client_counter += 1
+            
+        rtr = self.net.getNodeByName(self.simulation_topology.rtr)
+
+        # Gather statistics from the router:
+        q_proc = rtr.popen('python queue_statistics.py r-srv %s' % self.rtr_q_filename)
+        pcap_filename = os.path.join(self.res_dirname, "rtr_srv.pcap")
+        rtr.cmd('tshark -i r-srv -w %s -F libpcap&' % pcap_filename)
+
+        # Wait until all commands are completed:
+        for client, line in pmonitor(popens, timeoutms=1000):
+            if client:
+                print('<%s>: %s' % (client, line), )
+        # Kill the server's iperf -s processes, the router's queue monitor and tcpdumps:
+        sleep(10)
+        for process in srv_procs:
+            process.send_signal(SIGINT)
+        q_proc.send_signal(SIGINT)
+
+        self.net.stop()
+
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    num_of_reno = num_of_vegas = 4
+    setLogLevel('info')
+    # bw is in Mbps, delay in msec, queue size in packets
+    simulation_topology = SimulationTopology(num_of_reno, num_of_vegas, host_bw=100, srv_bw=200, srv_delay=15000, rtr_queue_size=10000)
+    simulation_name = "%s_reno_hosts_%s_vegas_hosts" % (str(num_of_reno), str(num_of_vegas))
+    simulator = Iperf3Simulator(simulation_topology, simulation_name, 10)
+    simulator.StartSimulation()
