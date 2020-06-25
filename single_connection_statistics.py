977f19300c3e3aeb55569a90112b506b28d04ef0
roymitrany <roym@technion.ac.il>
2020-06-25 10:14:30 +0300

    - some more fixes to the single connection statistics code

--- single_connection_statistics.py
+++ single_connection_statistics.py
@@ -9,66 +9,51 @@ from matplotlib import cycler
 from tcpdump_statistics import TcpdumpStatistics
 
 
-'''def fix_time(rounded_time_str):
-    z = datetime.strptime(rounded_time_str, "%H:%M:%S.%f")
-    time_in_tenth = int(10 * (z.hour * 3600 + z.minute * 60 + z.second + z.microsecond / 1000000))
-    date_time = datetime.fromtimestamp(time_in_tenth / 10)
-    time = date_time.strftime("%H:%M:%S.%f")[0:-5]
-    return time'''
-
-
 class SingleConnStatistics:
     def __init__(self, ingress_file_name, egress_file_name, rtr_q_filename, graph_file_name):
-        self.conn_df = self.create_dfs(ingress_file_name, egress_file_name, rtr_q_filename)
+        self.conn_df = self.create_df(ingress_file_name, egress_file_name, rtr_q_filename)
         print(self.conn_df)
         self.create_plots(graph_file_name)
 
-    def create_dfs(self, ingress_file_name, egress_file_name, rtr_q_filename):
-        in_dict = self.parse_dump_file(ingress_file_name)
-        out_dict = self.parse_dump_file(egress_file_name)
-        in_ser = pd.Series(in_dict)
-        out_ser = pd.Series(out_dict)
-        totals_df = pd.DataFrame({'in_total': in_ser, 'out_total': out_ser})
+    def create_df(self, ingress_file_name, egress_file_name, rtr_q_filename):
+        in_throughput_df = self.parse_dump_file(ingress_file_name)
+        out_throughput_df = self.parse_dump_file(egress_file_name)
+        connection_df = pd.concat([in_throughput_df, out_throughput_df], axis=1)  # Outer join between in and out df
+        connection_df.columns = ['In Throughput', 'in_total', 'Out Throughput', 'out_total']
 
-        throughput_df = totals_df.diff()
-        throughput_df.columns = ['In Throughput', 'Out Throughput']
-        throughput_df = throughput_df.div(100000 / 8)
-        totals_df = totals_df.join(throughput_df)
+        # The gap between the total in and the total out indicates what's in the queue
+        connection_df['Conn. Bytes in Queue'] = connection_df['in_total'] - connection_df['out_total']
 
-        totals_df['Conn. Bytes in Queue'] = totals_df['in_total'] - totals_df['out_total']
-
-        # Add qdisc columns, only for existinf keys
+        # Add qdisc columns, only for existing keys (inner join)
         qdisc_df = pd.read_csv(rtr_q_filename, sep="\t", header=None)
         qdisc_df.columns = ['Time', 'Total Bytes in Queue', 'Num of Packets', 'Num of Drops']
-        #qdisc_df['Time'] = qdisc_df['Time'].map(lambda time: fix_time(time))
         qdisc_df = qdisc_df.set_index('Time')
-        totals_df = totals_df.join(qdisc_df, lsuffix='_caller')
-        return totals_df
+        connection_df = connection_df.join(qdisc_df, lsuffix='_caller')
+        return connection_df
 
     @staticmethod
     def parse_dump_lines(lines):
-        totals_dict = {}
+        # totals_dict = {}
+        time_list = []
+        length_list = []
 
-        last_time_in_tenth = -1
-        total_bytes = 0
-        # Strips the newline character
-        count = 0
         for line in lines:
-            count += 1
-            if count % 1000 == 0:
-                print('&', end='')
-                if count % 30000 == 0:
-                    print(count)
             conn_index, time_str, length, ts_val = TcpdumpStatistics.parse_line(line)
             if int(length) == 0:  # ACK only, ignore
                 continue
-            total_bytes += int(length)
+
             # Take only 10th of a second from the time string:
             rounded_time_str = time_str[0:-5]
+            time_list.append(rounded_time_str)
+            length_list.append(float(length))
 
-            totals_dict[rounded_time_str] = total_bytes
+        df = pd.DataFrame({'Time': time_list, 'Thoughput': length_list})
+        df = df.groupby(['Time']).sum()
+        df.sort_index()
+        df['Total'] = df.cumsum()  # Add Total column that indicates the number of bytes passed so far
+        df['Thoughput'] = df['Thoughput'].map(lambda num: num * 8 / 100000)
 
-        return totals_dict
+        return df
 
     def parse_dump_file(self, file_name):
 
@@ -81,7 +66,6 @@ class SingleConnStatistics:
 
     def create_plots(self, graph_file_name):
 
-        print("=======CP1=============")
         fig, (throughput_ax, q_disc_ax) = plt.subplots(2, figsize=(10, 10))
         self.conn_df.plot(kind='line', ax=throughput_ax, y=['In Throughput', 'Out Throughput'],
                           title="Throughput vs. Bytes in Queue")
@@ -94,7 +78,6 @@ class SingleConnStatistics:
         ax4.set(ylabel='Bytes')
         self.conn_df.plot(kind='line', ax=ax4, y=['Conn. Bytes in Queue'])
         ax4.legend(loc=1)
-        print("=======CP2=============")
 
         self.conn_df.plot(kind='line', ax=q_disc_ax, y=['Num of Drops'], color="red")
         q_disc_ax.set(ylabel='Drops (pkts)')
@@ -105,21 +88,14 @@ class SingleConnStatistics:
         self.conn_df.plot(kind='line', ax=ax5, y=['Conn. Bytes in Queue', 'Total Bytes in Queue'],
                           title="Drops vs. Bytes in Queue")
         ax5.legend(loc=1)
-        print("=======CP3=============")
         plt.savefig(graph_file_name)
         plt.show()
 
     @staticmethod
     def reduce_lines(lines):
         # Take out all lines with length 0
-        count = 0
         conn_count = {}
         for line in lines:
-            count += 1
-            if count % 1000 == 0:
-                print('-', end='')
-                if count % 30000 == 0:
-                    print(count)
             # Ignore if length is 0
             if int(line.find('length 0')) > 0:  # ACK only, ignore
                 continue
@@ -132,15 +108,9 @@ class SingleConnStatistics:
         # extract the connection index
         our_conn_index = max(conn_count, key=conn_count.get)
 
-        count = 0
         reduced_lines = []
-        # loop on file again and remove all the lines that are not interesting
+        # loop on file again and add only the interesting lines to the list
         for line in lines:
-            count += 1
-            if count % 1000 == 0:
-                print('+', end='')
-                if count % 30000 == 0:
-                    print(count)
             conn_index, time_str, length, ts_val = TcpdumpStatistics.parse_line(line, {})
             if conn_index == our_conn_index:
                 reduced_lines.append(line)
@@ -149,18 +119,14 @@ class SingleConnStatistics:
 
         # 09:17:58.297429 IP 10.0.1.10.44848 > 10.0.10.10.5202: Flags [.], seq 1486:2934, ack 1, win 83, options [nop,nop,TS val 4277329349 ecr 645803186], length 1448
 
-    def reduce_retransmissions(self, lines):
-        # The method assumes single connection. If lines are from multiple connections, thwo messages
+    @staticmethod
+    def reduce_retransmissions(lines):
+        # The method assumes single connection. If lines are from multiple connections, two messages
         # from two different connections with the same seq number will be interpreted as retransmissions
+        # If data was sent multiple times, the method keeps only the last retransmission.
         reduced_lines = []
         transmission_dict = {}
-        count = 0
         for line in lines:
-            count += 1
-            if count % 1000 == 0:
-                print('^', end='')
-                if count % 30000 == 0:
-                    print(count)
             search_obj = re.search(r'.*seq (\d+).* length (\d+)', line)
             # All lines with no seq or no data should be automatically not reduced
             if search_obj is None:
@@ -172,6 +138,7 @@ class SingleConnStatistics:
                 continue
 
             seq = int(search_obj.group(1))
+            # Map the line to the sequence. If older sequence was there, it will be automatically reduced.
             transmission_dict[seq] = line
         return reduced_lines + list(transmission_dict.values())
 
@@ -186,8 +153,8 @@ if __name__ == '__main__':
         rtr_file = "results/%s/rtr_q.txt" % test_name
         graph_file_name = "results/%s/BIQ_%s.png" % (test_name, host_name)
     else:
-        in_file = "test_files/in_file_test.txt"
-        out_file = "test_files/out_file_test.txt"
+        in_file = "test_files/client_reno_0.txt"
+        out_file = "test_files/server_reno_0.txt"
         rtr_file = "test_files/rtr_q.txt"
         graph_file_name = "test_files/BIQ.png"
     # in_file = "test_files/in_short.txt"
