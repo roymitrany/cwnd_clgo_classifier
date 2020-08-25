269bb4db03443ed1a60ef325c8d0e55bb7840073
deanc <deancarmel95@gmail.com>
2020-08-25 14:51:40 +0300

    added environment file.

---
+++ learning/env.py
@@ -0,0 +1,5 @@
+absolute_path = "C:\\Users\\roym\\PycharmProjects\\cwnd_clgo_classifier\\"
+training_files_path = absolute_path + "results"
+training_parameters_path = absolute_path + "cnn_training_parameters"
+testing_files_path = r'C:\Users\deanc\PycharmProjects\Congestion_Control_Classifier\test_files2'
+# training_files_path = r'C:\Users\deanc\PycharmProjects\Congestion_Control_Classifier\train_files2'
