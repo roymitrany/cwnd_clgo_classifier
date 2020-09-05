2856b6efd04361b6a4189e89851e94f4c84f744f
deanc <deancarmel95@gmail.com>
2020-09-05 17:51:15 +0300

    3 hosts classification examination.

--- learning/env.py
+++ learning/env.py
@@ -1,5 +1,6 @@
 import os
 absolute_path = r'C:\Users\deanc\PycharmProjects\Congestion_Control_classifier_GitHub\\'
 training_files_path = os.path.join(absolute_path, 'train_files')
-training_parameters_path = os.path.join(absolute_path, 'cnn_training_parameters\\')
-testing_files_path = os.path.join(absolute_path, 'test_files')
+training_parameters_path = os.path.join(absolute_path, 'cnn_training_parameters_10\\')
+testing_files_path = os.path.join(absolute_path, 'test_files_10')
+testing_results_path = os.path.join(absolute_path, 'test_results')
